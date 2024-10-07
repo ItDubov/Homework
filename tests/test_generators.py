0c4d08cf6dd5d088631d4f1aece93fd3cfff217e
Виктор <viktor.dubov.2007@gmail.com>
2024-10-07 14:41:34 +0300

    Добавление декоратора и тестов к ниму

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -24,6 +24,7 @@ transactions = [
     }
 ]
 
+
 def test_filter_by_currency_usd():
     usd_transactions = filter_by_currency(transactions, "USD")
     result = list(usd_transactions)
@@ -31,10 +32,12 @@ def test_filter_by_currency_usd():
     assert result[0]["id"] == 1
     assert result[1]["id"] == 3
 
+
 def test_filter_by_currency_empty():
     empty_transactions = filter_by_currency(transactions, "EUR")
     result = list(empty_transactions)
     assert result == []
+
 
 transactions_with_descriptions = [
     {"description": "Перевод организации"},
@@ -42,16 +45,19 @@ transactions_with_descriptions = [
     {"description": "Оплата услуг"},
 ]
 
+
 def test_transaction_descriptions():
     descriptions = transaction_descriptions(transactions_with_descriptions)
     assert next(descriptions) == "Перевод организации"
     assert next(descriptions) == "Перевод со счета на счет"
     assert next(descriptions) == "Оплата услуг"
 
+
 def test_transaction_descriptions_empty():
     empty_descriptions = transaction_descriptions([])
     with pytest.raises(StopIteration):
         next(empty_descriptions)
+
 
 def test_card_number_generator():
     generator = card_number_generator(1, 3)
@@ -59,9 +65,9 @@ def test_card_number_generator():
     assert next(generator) == "0000 0000 0000 0002"
     assert next(generator) == "0000 0000 0000 0003"
 
+
 def test_card_number_generator_range():
     generator = card_number_generator(9999999999999997, 9999999999999999)
     assert next(generator) == "9999 9999 9999 9997"
     assert next(generator) == "9999 9999 9999 9998"
     assert next(generator) == "9999 9999 9999 9999"
-
